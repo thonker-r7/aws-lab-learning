6d2e3f7fb6dd88b458dd820da6c4e8fb38023d9b
Tim H <23639701+thonker-r7@users.noreply.github.com>
2019-11-11 14:08:48 -0500

    initial upload - not tested for long term use
    Auto update processes might kick off, only had EC2 instance on for an hour or two

---
+++ bootstrap_ubuntu_disable_autoupdates.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Bootstrap script to disable unattended upgrades on ubuntu systems
+# Designed for checking for vulns, ensure accuracy of findings for base AMI, not a patched version
+# Written by Tim Honker 2019
+
+# Stop the service ASAP
+service unattended-upgrades stop
+
+# Wait a second just in case
+sleep 1
+
+# Forcibly kill it if it is still running
+pkill -f "unattended-upgrades"
+
+# grab the list of updates, but do not actually update. Often needed before you can do a remove
+apt-get update -qq
+
+# uninstall the package to prevent it from happening on reboot or ever
+apt-get remove -qq -y unattended-upgrades
+
+# leave a market in user home directories to let them know this script ran
+touch /root/updates-have-been-disabled.txt /home/ubuntu/updates-have-been-disabled.txt
+
+
+# Additional optional things that weren't needed at least for the short power on/off test.
+# after a test run, the following directory had all logs as 0 bytes and hadn't been changed since power on
+#  /var/log/unattended-upgrades/
+#
+# Kill any other APT processes from autostarting
+#rm -f /etc/apt/apt.conf.d/20auto-upgrades
+#rf -f /etc/apt/apt.conf.d/50unattended-upgrades
